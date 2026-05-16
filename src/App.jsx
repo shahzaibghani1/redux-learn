20e0e84a2e34917011b7dfa64f13a22364332a60
shahzaib <shahzaibghani.official@gmail.com>
2026-05-16 22:42:55 +0500

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div className='font-bold text-center p-5'>Hello World</div>
+    </>
+  )
+}
+
+export default App
